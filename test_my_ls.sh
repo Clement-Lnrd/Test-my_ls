cd4c78ffccb67ebc648bf41e6ce6fb16201e0162
Clement-Lnrd <clement.lienard@epitech.eu>
2022-11-10 14:40:05 +0100

    [~] edit test_my_ls.sh: add tests and edit script base

--- test_my_ls.sh
+++ test_my_ls.sh
@@ -4,10 +4,8 @@
 
 dir=$HOME/.tmp_tests_my_ls
 me=`basename "$0"`
-
-reload_perms () {
-  sudo chmod -R 666 dir/
-}
+PS3='Select an option (1-3): '
+options=("Continue, I already running as sudo" "Stop" "Continue anyway (/!\\ WARNING: test risk return everything is good, but that may not be the case /!\\)")
 
 # Header
 
@@ -27,58 +25,71 @@ echo -e "\e[31m\033[1m
 #          \/\/                  \/          \/      \/     \/     \/                   \/ \/     #
 #                                                                                                 #
 ###################################################################################################\033[0m\n"
-echo -e "\e[32mIf the program asks for your sudo password, don't worry, it's to avoid permissions issues\033[0m\n"
-read -p "Press enter to continue"
+echo -e "Please relaunch script as ('./$me') if you are not already, you risk permissions issues\n"
+select fav in "${options[@]}"; do
+    case $fav in
+        "Continue, I already running as sudo")
+            break
+            ;;
+        "Stop")
+            exit 0
+            ;;
+        "Continue anyway (/!\\ WARNING: test risk return everything is good, but that may not be the case /!\\)")
+            break
+	    break
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 echo ""
 
 # Recompile with Makefile
 
 make re > /dev/null
 
-# Create temporary directory for tests
+# Create temporary directory and files for tests
 
-if ! mkdir dir; then
-    echo -e "\e[31m\033[1m[ERROR] Failed to create temporary directory, already exist"
-    echo -e "        Please delete it or rename in script\033[0m\n"
+if ! mkdir $dir; then
+    echo -e "\e[31m\033[1m[ERROR] Failed to create temporary directory, already exist or permission issue\n        Please delete it, rename in script or relaunch as sudo\033[0m\n"
     exit 84
 fi
-reload_perms
 
 # Send ls & my_ls outputs in files in the temporary directory
 
-ls > dir/ls
-./my_ls > dir/my_ls
-reload_perms
+ls > $dir/ls
+./my_ls > $dir/my_ls
+ls -l > $dir/ls_l
+./my_ls -l > $dir/my_ls_l
+ls -a > $dir/ls_a
+./my_ls -a > $dir/my_ls_a
+ls -la > $dir/ls_la
+./my_ls -la > $dir/my_ls_la
 
 # Compare outputs
 
-diff dir/ls dir/my_ls > dir/diff
-echo -e "\e[36m\033[1m//Test#1\033[0m"
-echo -e "\e[36mls / ./my_ls"
-if [ -s dir/diff ]; then
-    echo -e "Outputs are differents"
-    cat dir/diff
+diff $dir/ls $dir/my_ls > $dir/diff
+diff $dir/ls_a $dir/my_ls_a > $dir/diff_a
+echo -e "\e[36m\033[1m//Test #1\033[0m\033[36m\nls [without OPTION/FILE]"
+if [ -s $dir/diff ]; then
+    echo -e "Outputs are differents:\033[0m"
+    cat $dir/diff
 else
-    echo -e "All is good\033[0m"
+    echo -e "\e[36mAll is good\033[0m"
+fi
+echo ""
+echo -e "\e[36m\033[1m//Test #2\033[0m\033[36m\nls -a"
+if [ -s $dir/diff_a ]; then
+    echo -e "Outputs are differents:\033[0m"
+    cat $dir/diff_a
+else
+    echo -e "\e[36mAll is good\033[0m"
 fi
 
 # Delete temporary files & directory
 
-if ! rm dir/ls; then
-    echo -e "\e[31m\033[1m[ERROR] Failed to delete temporary file ($dir/ls)"
-    echo -e "        Try 'sudo ./$me'\033[0m\n"
-fi
-if ! rm dir/my_ls; then
-    echo -e "\e[31m\033[1m[ERROR] Failed to delete temporary file ($dir/my_ls)"
-    echo -e "        Try 'sudo ./$me'\033[0m\n"
-fi
-if ! rm dir/diff; then
-    echo -e "\e[31m\033[1m[ERROR] Failed to delete temporary file ($dir/diff)"
-    echo -e "        Try 'sudo ./$me'\033[0m\n"
-fi
-if ! rmdir dir; then
-    echo -e "\e[31m\033[1m[ERROR] Failed to delete temporary directory, not empty"
-    echo -e "        Please empty it or delete yourself\033[0m\n"
+if ! rm -r $dir; then
+    echo -e "\e[31m\033[1m[ERROR] Failed to delete temporary directory\n        Please delete it yourself\033[0m\n"
 fi
 
 exit 0
